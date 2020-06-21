50d31a6a8c4c6d8e6e00306a31a896884cf1ebd0
Jonas <jonasp@ciandt.com>
2020-06-21 16:28:32 -0300

    WIP

---
+++ app/upload-arquivo/upload-arquivo.component.ts
@@ -0,0 +1,55 @@
+import { Component, ViewChild, ElementRef, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+@Component({
+    selector: 'app-upload-arquivo',
+    templateUrl: './upload-arquivo.component.html',
+    styleUrls: ['./upload-arquivo.component.scss']
+})
+export class UploadArquivoComponent implements OnInit, OnDestroy {
+
+    @ViewChild('fileUpload') fileUpload: ElementRef;
+    @Input() accept: string;
+    @Input() status: {
+        progress: number;
+    };
+    @Input() resetObservable: Observable<boolean>;
+    @Output() obterArquivo = new EventEmitter();
+    @Output() enviarArquivo = new EventEmitter<File>();
+    $resetSubs: Subscription;
+    arquivo: File;
+
+    ngOnInit() {
+        this.$resetSubs = this.resetObservable.subscribe((reset: boolean) => {
+            if (reset) {
+                this.onReset();
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        this.$resetSubs.unsubscribe();
+    }
+
+    abrir() {
+        this.fileUpload.nativeElement.click();
+        this.fileUpload.nativeElement.onchange = () => {
+            this.arquivo = this.fileUpload.nativeElement.files[0];
+        };
+        this.obterArquivo.emit(this.arquivo);
+    }
+
+    hasFile() {
+        return this.fileUpload.nativeElement.files.length > 0;
+    }
+
+
+    clickUpload() {
+        this.enviarArquivo.emit(this.arquivo);
+    }
+
+    onReset() {
+        this.fileUpload.nativeElement.value = null;
+        this.obterArquivo.emit(null);
+        this.status.progress = 0;
+    }
+}
